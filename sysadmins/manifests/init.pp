9b2b7351aff3246fde73b26c3d030e3306229bf7
jackd85 <durhamj85@gmail.com>
2020-10-10 09:10:55 -0400

    add additional users

--- sysadmins/manifests/init.pp
+++ sysadmins/manifests/init.pp
@@ -10,4 +10,17 @@ class sysadmins {
     uid    => '9999',
     groups => 'sysadmins',
  }
+ 
+ user { 'susan':
+    ensure => present,
+    uid    => '9998',
+    groups => 'sysadmins',
+ }
+
+ user { 'peter':
+    ensure => present,
+    uid    => '9997',
+    groups => 'sysadmins',
+ }
+  
 }
